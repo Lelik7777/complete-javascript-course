4dc819c6c8a0591b052c61dd7ef83059f012d5f6
Lelik Jan <21alex7777@gmail.com>
2022-09-02 20:15:18 +0300

    feat: create strict_mode.js

---
+++ 02-Fundamentals-Part-2/strict mode/strict_mode.js
@@ -0,0 +1,10 @@
+'use strict';
+//a=34;
+//console.log(typeof a);
+
+let hasLicense=false;
+const passTest=true;
+if(passTest) hasLicens=false;
+//reserved words throw errors in strict mode
+//const interface='bob';
+//const private=35;
