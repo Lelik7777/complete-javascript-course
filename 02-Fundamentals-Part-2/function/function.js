e89a4ac8ccbaa19f44b3a52ce6d2f6135468fd47
Lelik Jan <21alex7777@gmail.com>
2022-09-05 21:59:46 +0300

    feat:  in function.js create several functions

--- 02-Fundamentals-Part-2/function/function.js
+++ 02-Fundamentals-Part-2/function/function.js
@@ -2,9 +2,20 @@ console.log('hello,function');
 
 function f() {
     console.log('empty function');
-   // return 'string';
+    // return 'string';
 }
 
-let result=f();//undefined
-if(result) console.log('function return something');
+let result = f();//undefined
+if (result) console.log('function return something');
 console.log(result);
+
+const calcAverage = scores => scores.reduce((acc, cur) => acc + cur) / scores.length;
+const scoreAverageDolphins = calcAverage([85, 54, 41]);
+console.log(scoreAverageDolphins);
+const scoreAverageKoalas = calcAverage([23, 34, 27]);
+const checkWinner = (averageDolphis, averageKoalas) => {
+    const noWin = 'nobody wins';
+    if (averageDolphis >= averageKoalas) return averageDolphis >= averageKoalas * 2 ? 'dolphins wins' : noWin;
+    if (averageDolphis < averageKoalas) return averageKoalas >= averageDolphis * 2 ? 'koalas wins' : noWin;
+}
+console.log(checkWinner(scoreAverageDolphins, scoreAverageKoalas))
