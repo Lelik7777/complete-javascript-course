c3f04c6d8c0e1345a7e60f1c4ef4fa86b259a3bd
Lelik Jan <21alex7777@gmail.com>
2022-09-09 22:26:54 +0300

    feat: add developer-skills/start/script.js some functions

--- 02-Fundamentals-Part-2/loop/loop.js
+++ 02-Fundamentals-Part-2/loop/loop.js
@@ -27,3 +27,45 @@ while (dice != 6) {
     dice = Math.trunc(Math.random() * 6 + 1);
     dice === 6 && console.log('loop is about to end...');
 }
+;
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+function calcTip(arr) {
+    arr.forEach((x, i) => {
+        if (x >= 50 && x <= 300) {
+            tips.push(x * .15);
+            totals.push(tips[i] + x);
+        } else {
+            tips.push(x * .2);
+            totals.push(tips[i] + x);
+        }
+    });
+    return {
+        tips,
+        totals,
+    }
+};
+console.log(calcTip(bills));
+const calcAverage = (arr) => {
+    let sum = 0;
+    for (const el of arr) {
+        sum += el;
+    }
+    console.log(sum / arr.length);
+    return arr.reduce((a, c) => a + c) / arr.length;
+}
+console.log(calcAverage(totals));
+for (const el of totals) {
+    var a=0;
+a++;
+}
+console.log(a);
+let b=0;
+while (b<5){
+     var c=1;
+    c+=1;
+    b++;
+}
+console.log(c);
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,63 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+console.log('hello');
+console.log('some text');
+console.log('added text');
+const temperatures = [-3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const temperatures2=[-19,-23,-1,'error',34,3,21,43];
+console.log([...temperatures,...temperatures2]);
+const findMaxValue = (arr) => {
+    let max = arr[1];
+    arr.forEach((x, i) => {
+        if (x >= max) max = x;
+    });
+    return max;
+}
+const findMinValue = (arr) => {
+    let min = arr[0];
+    arr.forEach(x => {
+        if (x <= min) min = x;
+    });
+    return min;
+}
+const checkArr = (arr) => {
+    const copy = [...arr];
+    arr.forEach(x => {
+        if (typeof x === "number") copy.push(x);
+    });
+    return copy;
+};
+console.log('copy: ', checkArr(temperatures))
+const amplitudeTemper = (arrTemper,...rest) => {
+    console.log('rest',rest)
+    let copy = [...arrTemper,...rest.flat(1)];
+    console.log('copy from 33:',copy);
+    copy.forEach((x, i) => {
+        if (typeof x !== 'number') copy.splice(i, 1);
+    });
+    console.log('copy from 36',copy)
+    //custom variant getting copy
+    const copy2 = checkArr(copy);
+    const maxValue = Math.max(...copy);
+    const minValue = Math.min(...copy);
+    //custom function for max and min value
+    const maxValue1 = findMaxValue(copy2);
+    const minValue2 = findMinValue(copy2);
+    console.log(maxValue);
+    console.log(minValue);
+    console.log(copy)
+    return {
+        'amplitude from built-in functions': maxValue-minValue,
+        'amplitude from custom functions': maxValue1 - minValue2,
+    }
+};
+console.log('result is ', amplitudeTemper(temperatures,temperatures2,[4,5,5,50,64]));
+function fun(arr,...rest) {
+    return [...arr,...rest.flat(1)]
+}
+
+console.log(fun([3,4,5],[6,7,8],[9,10,11]));
+
+
+
 
