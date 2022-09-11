14097a2c279e59d430dc6dfe552451a24200e42e
Lelik Jan <21alex7777@gmail.com>
2022-09-11 19:54:13 +0300

    feat: add developer-skills/start/script.j  functions to convert celsius to kelvin

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -4,8 +4,8 @@ console.log('hello');
 console.log('some text');
 console.log('added text');
 const temperatures = [-3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
-const temperatures2=[-19,-23,-1,'error',34,3,21,43];
-console.log([...temperatures,...temperatures2]);
+const temperatures2 = [-19, -23, -1, 'error', 34, 3, 21, 43];
+console.log([...temperatures, ...temperatures2]);
 const findMaxValue = (arr) => {
     let max = arr[1];
     arr.forEach((x, i) => {
@@ -28,14 +28,14 @@ const checkArr = (arr) => {
     return copy;
 };
 console.log('copy: ', checkArr(temperatures))
-const amplitudeTemper = (arrTemper,...rest) => {
-    console.log('rest',rest)
-    let copy = [...arrTemper,...rest.flat(1)];
-    console.log('copy from 33:',copy);
+const amplitudeTemper = (arrTemper, ...rest) => {
+    console.log('rest', rest)
+    let copy = [...arrTemper, ...rest.flat(1)];
+    console.log('copy from 33:', copy);
     copy.forEach((x, i) => {
         if (typeof x !== 'number') copy.splice(i, 1);
     });
-    console.log('copy from 36',copy)
+    console.log('copy from 36', copy)
     //custom variant getting copy
     const copy2 = checkArr(copy);
     const maxValue = Math.max(...copy);
@@ -47,16 +47,39 @@ const amplitudeTemper = (arrTemper,...rest) => {
     console.log(minValue);
     console.log(copy)
     return {
-        'amplitude from built-in functions': maxValue-minValue,
+        'amplitude from built-in functions': maxValue - minValue,
         'amplitude from custom functions': maxValue1 - minValue2,
     }
 };
-console.log('result is ', amplitudeTemper(temperatures,temperatures2,[4,5,5,50,64]));
-function fun(arr,...rest) {
-    return [...arr,...rest.flat(1)]
+console.log('result is ', amplitudeTemper(temperatures, temperatures2, [4, 5, 5, 50, 64]));
+
+function fun(arr, ...rest) {
+    return [...arr, ...rest.flat(1)]
 }
 
-console.log(fun([3,4,5],[6,7,8],[9,10,11]));
+console.log(fun([3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 13, 14], 15, [16, 17, 18]));
+
+//function to perform measurement in kelvin
+const convertKelvin = (celsius) => {
+    return celsius + 273;
+}
+temperatures.forEach(x => console.log(`${x} celsius = ${convertKelvin(x)} kelvin`));
+const measureKelvin = () => {
+    const measurement = {
+        type: 'temperature',
+        unit: 'celsius',
+        value: +prompt('enter temperature in celsius'),
+    }
+    console.table(measurement)
+    return measurement.value + 273;
+}
+console.log(` temperature in kelvin is ${measureKelvin()}`);
+console.table([3,['hello',43,'world'],'bob',true,{name:'bob'}]);
+
+console.log('17°C');
+
+
+
 
 
 
