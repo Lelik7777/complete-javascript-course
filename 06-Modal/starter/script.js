6577560c4de7859e901c8dff5d7544a09c47d5a2
Lelik Jan <21alex7777@gmail.com>
2022-09-14 11:29:20 +0300

    feat: refactoring  in 05-Guess-My-Number/starter/script.js: feature - capture the event on body level and check class name of element that create event

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+const $btnClosedModal = document.querySelector('.close-modal');
+const $btnShowModal = document.querySelectorAll('.show-modal');
+const $overlay = document.querySelector('.overlay');
+const $modal = document.querySelector('.modal');
+
+console.log($btnShowModal)
+
+function showElement(token) {
+    $modal.classList.remove(token);
+    $overlay.classList.remove(token);
+}
+
+function closeElement(token) {
+    $modal.classList.add(token);
+    $overlay.classList.add(token);
+}
+
+// for (const el of $btnShowModal) {
+//     console.log(el)
+//     el.addEventListener('click', () => {
+//         showElement('hidden');
+//
+//     });
+// }
+//моя реализация всплытия события и его поимка на уровне body с проверкой
+// имени класса элемента,где это событие возникло
+document.body.addEventListener('click', (ev) => {
+    console.log(ev.target)
+    ev.target.classList.value==='show-modal'&& showElement('hidden');
+});
+$btnClosedModal.addEventListener('click', () => {
+    closeElement('hidden');
+});
+
+
