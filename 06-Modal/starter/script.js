18d3f2424974c873d582beffbb7a222b0360d98e
Lelik Jan <21alex7777@gmail.com>
2022-09-14 12:51:00 +0300

    feat: refactoring  in 05-Guess-My-Number/starter/script.js: implement close modal window by press esc and click outside the modal window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -27,11 +27,18 @@ function closeElement(arr, token) {
 //моя реализация всплытия события и его поимка на уровне body с проверкой
 // имени класса элемента,где это событие возникло
 document.body.addEventListener('click', (ev) => {
-    ev.target.classList.value === 'show-modal' && showElement(arrElements,'hidden');
-    ev.target.classList.value === 'close-modal' && closeElement(arrElements, 'hidden');
+    ev.target.classList.value === 'show-modal' && showElement(arrElements, 'hidden');
+    //use method .contains that more better for reading and understanding
+    ev.target.classList.contains('close-modal') && closeElement(arrElements, 'hidden');
+    //implement closing modal window when click outside the this one
+    ev.target.classList.contains('overlay') && closeElement(arrElements, 'hidden');
 });
 // $btnClosedModal.addEventListener('click', () => {
 //     closeElement('hidden');
 // });
+document.body.addEventListener('keydown', (ev) => {
+    ev.code === 'Escape' && closeElement(arrElements, 'hidden');
+});
+
 
 
