5d8d2f28638fa97aa111bcc04febc2d9887fa1cc
Lelik Jan <21alex7777@gmail.com>
2022-09-20 19:01:35 +0300

    feat:scope chain

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -5,4 +5,30 @@
         return a+b;
     }
 }
-console.log(sum(4,5));//reference error
+//console.log(sum(4,5));//reference error
+
+
+const fun1 = (a,b) => {
+  const fun2 = () => {
+    return a+b;
+  }
+    console.log(fun2());
+  return fun2();
+}
+const a=fun1(4,5);
+console.log(a);
+
+const name='jonas';
+first();
+function first() {
+    const b='hello';
+    second();
+    function second() {
+        const c='hi';
+        third();
+    }
+}
+function third() {
+    const d='hey!';
+    console.log(name)
+}
