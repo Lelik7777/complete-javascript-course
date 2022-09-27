7ad4ff75788563eeca718b0c201df308ea0f4b7f
Lelik Jan <21alex7777@gmail.com>
2022-09-27 22:33:24 +0300

    feat: array methods: splice(),slice(),at(),

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -250,3 +250,17 @@ const boardPassengers = function (n, wait) {
 const perGroup = 1000;
 boardPassengers(180, 3);
 
+
+//challenge #2
+(function () {
+    const header=document.querySelector('h1');
+    header.style.color='red';
+    let toggle=true;
+    console.log(toggle);
+    document.querySelector('body').addEventListener('click',function () {
+        if(toggle) header.style.color='blue';
+        else header.style.color='red';
+        toggle=!toggle;
+        console.log(toggle);
+    })
+})();
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -6,31 +6,31 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -66,11 +66,26 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // LECTURES
 
 const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+//slice()
+const arrayLetters = ['a', 'b', 'c', 'd', 'e', 'g', 'f'];
+console.log(arrayLetters.slice(1, 3));
+console.log(arrayLetters.slice(-2));
+console.log(arrayLetters.slice(2, -2));
+
+//splice
+//delete last array element
+//arrayLetters.splice(-1);
+console.log(arrayLetters);
+
+//.at() return string
+//get last element
+console.log( arrayLetters.at(-1));
+console.log('hello'.at(-1));
