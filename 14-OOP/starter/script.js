e476d4064511609196fa0cd4fdd42b5d8dc853c2
Lelik Jan <21alex7777@gmail.com>
2022-10-12 12:09:37 +0300

    feat: using constructor function

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+let log=console.log;
+log('hello');
+
+function Person(name,age) {
+    console.log('indicate to',this);
+    this.name=name;
+    this.age=age;
+    //так делать нельзя,поскольку засоряется память
+    this.getAge=function () {
+        return this.age;
+    }
+}
+
+console.log(new Person('bob',33));
+const bob=new Person('bob',33);
+console.log(bob);
+console.log(bob.name)
+console.log(bob.getAge())
+
+class PersonClass {
+    constructor(name,age) {
+        console.log(this)
+        this.name=name;
+        this.age=age;
+    }
+}
+
+console.log( new PersonClass('tom',44))
