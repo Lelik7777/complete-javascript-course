f8fcc4db2e05ff46cca82471e0b8a7e6cb4533a3
Lelik Jan <21alex7777@gmail.com>
2022-10-14 19:51:13 +0300

    feat: using  leaflet

---
+++ 15-Mapty/starter/other.js
@@ -0,0 +1,2 @@
+const globalVar='it`s a global variable for script.js';
+//console.log(globalVar)
--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -16,9 +16,20 @@ const $inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
         const {latitude, longitude} = position.coords;
+        const coords=[latitude,longitude];
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
         console.log(`https://www.google.pl/maps/@${latitude},${longitude},6z`)
     }, function () {
         console.log('could not get your position')
     })
 }
 
+console.log(globalVar)
