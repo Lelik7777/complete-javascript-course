28e689eb3dc393db2a0b19429bc10c3c5b72b4a0
Lelik Jan <21alex7777@gmail.com>
2022-10-14 18:42:18 +0300

    feat:get my geolocation on Google map

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -3,10 +3,22 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
+const $form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const $inputType = document.querySelector('.form__input--type');
+const $inputDistance = document.querySelector('.form__input--distance');
+const $inputDuration = document.querySelector('.form__input--duration');
+const $inputCadence = document.querySelector('.form__input--cadence');
+const $inputElevation = document.querySelector('.form__input--elevation');
+
+//get local position from google map
+// я скопировал ссылку гугл мэп и вставил в нее свои данные из геолокации
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const {latitude, longitude} = position.coords;
+        console.log(`https://www.google.pl/maps/@${latitude},${longitude},6z`)
+    }, function () {
+        console.log('could not get your position')
+    })
+}
+
