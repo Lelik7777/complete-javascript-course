f9116d9906cf4f3357477d5644c44a56b4239719
Lelik Jan <21alex7777@gmail.com>
2022-10-18 22:19:06 +0300

    feat: create class App and instance app

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,38 +11,90 @@ const $inputDuration = document.querySelector('.form__input--duration');
 const $inputCadence = document.querySelector('.form__input--cadence');
 const $inputElevation = document.querySelector('.form__input--elevation');
 
-//get local position from google map
-// я скопировал ссылку гугл мэп и вставил в нее свои данные из геолокации
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
+// get global variables
+//let map, eventMap;
+
+//create architecture by making class App
+class App {
+    #map;
+    #eventMap;
+
+    constructor() {
+        this.#getPosition();
+        $form.addEventListener('submit', this.#newWorkout.bind(this));
+//realize toggle in input type
+        $inputType.addEventListener('change',this.#toggleElevationField);
+    }
+
+    #getPosition() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.#loadMap.bind(this), function () {
+                console.log('could not get your position');
+            })
+        }
+    }
+
+    #loadMap(position) {
         const {latitude, longitude} = position.coords;
         const coords = [latitude, longitude];
-        const map = L.map('map').setView(coords, 13);
+        // using library  leaflet
+        this.#map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        }).addTo(this.#map);
 
+//вешаю на map обработчкие события из коробки leaflet
+        this.#map.on('click', this.#showForm.bind(this))
+    }
 
-        map.on('click', function (eventMap) {
-            const {lat, lng} = eventMap.latlng;
-            L.marker([lat, lng], {
-                opacity: 0.9,
-            }).addTo(map)
-                .bindPopup(L.popup({
-                    maxWidth: 250,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: 'running-popup'
-                }))
-                .setPopupContent('workout')
-                .openPopup();
-        })
+    #showForm(eventM) {
+        this.#eventMap = eventM;
 
-    }, function () {
-        console.log('could not get your position');
-    })
+        //whenever click on map, appear form on left
+        $form.classList.remove('hidden');
+        //фокус сразу же на input distance
+        $inputDistance.focus();
+    }
+
+    #toggleElevationField() {
+
+        $inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        $inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+
+    }
+
+    #newWorkout(e) {
+        e.preventDefault();
+        //clear inputs
+        $inputCadence.value = $inputDuration.value = $inputDistance.value = $inputElevation.value = '';
+        // отрисовка маркера
+        const {lat, lng} = this.#eventMap.latlng;
+        L.marker([lat, lng], {
+            opacity: 0.9,
+        }).addTo(this.#map)
+            // меняю поведение маркера при клике на карте
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent('workout')
+            .openPopup();
+    }
+
 }
-//get data from another script
+
+const app = new App();
+
+//get local position from google map
+// я скопировал ссылку гугл мэп и вставил в нее свои данные из геолокации
+
+
+//form submit
+
+//get data from other script  - я его создал специально,чтобы посмотреть доступ к глобальным переменным
 console.log(globalVar);
+
