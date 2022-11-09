b105f70dbb07771900047c3e4b62c1df07660b6f
Lelik Jan <21alex7777@gmail.com>
2022-11-09 19:14:32 +0300

    feat: using functional programming

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -13,8 +13,8 @@ const spendingLimits = Object.freeze({
     jonas: 1500,
     matilda: 100,
 });
-const getLimit = user => spendingLimits?.[user] ?? 0;
-const addExpense = function (state, limit, value, description, user = 'jonas') {
+const getLimit = (user, limits) => limits?.[user] ?? 0;
+const addExpense = function (state, limits, value, description, user = 'jonas') {
     //using parameter by default
     //if (!user) user = 'jonas';
     const cleanUser = user.toLowerCase();
@@ -30,7 +30,7 @@ const addExpense = function (state, limit, value, description, user = 'jonas') {
     // value <= getLimit(cleanUser) && budget.push({value: -value, description, user:cleanUser});
     //здесь мы реализуем принцип чистой ф-ции,которая не мутирует внешние данные,а работает с их копиями
     //если условие неверно,то возвращаем старый state
-    return value <= getLimit(cleanUser) ? [...state, {value: -value, description, user: cleanUser}] : state;
+    return value <= getLimit(limits, cleanUser) ? [...state, {value: -value, description, user: cleanUser}] : state;
 
 };
 const budget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
@@ -38,37 +38,44 @@ const budget2 = addExpense(budget1, spendingLimits, 100, 'Going to movies 🍿',
 const budget3 = addExpense(budget2, spendingLimits, 200, 'Stuff', 'Jay');
 console.log(budget2);
 
-const checkExpenses = function () {
-    // for (let entry of budget) {
-    //     // const limit = spendingLimits?.[entry.user] ?? 0;
-    //     // if (spendingLimits[entry.user]) {
-    //     //     lim = spendingLimits[entry.user];
-    //     // } else {
-    //     //     lim = 0;
+const checkExpenses = (state, limits) => state.map(entry => entry.value < -getLimit(entry.user, limits) ? {
+    ...entry,
+    flag: 'limit'
+} : entry);
+// for (let entry of budget) {
+//     // const limit = spendingLimits?.[entry.user] ?? 0;
+//     // if (spendingLimits[entry.user]) {
+//     //     lim = spendingLimits[entry.user];
+//     // } else {
+//     //     lim = 0;
+//     // }
+//
+//     if (entry.value < -getLimit(entry.user)) {
+//         entry.flag = 'limit';
+//     }
+// }
+
+
+const budget4 = checkExpenses(budget3, spendingLimits);
+
+console.log(budget4);
+
+const logBigExpenses = function (bigLimit,state) {
+    const bigExprneses= state.filter(entry=>entry.value<=-bigLimit)
+        .map(entry=>entry.description.slice(-2)).join(' / ');
+    console.log(bigExprneses);
+    // let output = '';
+    // for (const entry of budget) {
+    //     // if (entry.value <= -bigLimit) {
+    //     //     //output += entry.description.slice(-2) + ' / '; // Emojis are 2 chars
+    //     //     output+=`${entry.description.slice(-2)} / `
     //     // }
     //
-    //     if (entry.value < -getLimit(entry.user)) {
-    //         entry.flag = 'limit';
-    //     }
+    //     output += entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
     // }
-    budget = budget.map(entry => entry.value < -getLimit(entry.user) ? {...entry, flag: 'limit'} : entry);
+    // output = output.slice(0, -2); // Remove last '/ '
+    //
+    // console.log(output);
+    //
 };
-checkExpenses();
-
-console.log(budget);
-
-const logBigExpenses = function (bigLimit) {
-    let output = '';
-    for (const entry of budget) {
-        // if (entry.value <= -bigLimit) {
-        //     //output += entry.description.slice(-2) + ' / '; // Emojis are 2 chars
-        //     output+=`${entry.description.slice(-2)} / `
-        // }
-
-        output += entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
-    }
-    output = output.slice(0, -2); // Remove last '/ '
-
-    console.log(output);
-};
-logBigExpenses(500);
+logBigExpenses(10,budget4);
